6e7251520b92e5a96d05df019e5dac00c9c719a2
Mario0051 <windowsmario64@gmail.com>
2025-10-28 19:37:39 +0000

    Update interceptor_impl.rs

--- src/ios/interceptor_impl.rs
+++ src/ios/interceptor_impl.rs
@@ -1,53 +1,71 @@
+use crate::core::error::Error;
+use crate::core::interceptor::HookHandle;
+use std::ffi::{c_void, CString};
+use std::ptr;
 
+pub unsafe fn hook(orig_addr: usize, hook_addr: usize) -> Result<usize, Error> {
+    let trampoline = dobby_rs::hook(
+        orig_addr as *mut c_void,
+        hook_addr as *mut c_void
+    )?;
+    Ok(trampoline as usize)
+}
 
-use crate::core::interceptor::{Interceptor, InterceptorError};
-use std::ffi::CString;
+pub unsafe fn hook_vtable(
+    vtable: *mut usize,
+    vtable_index: usize,
+    hook_addr: usize,
+) -> Result<HookHandle, Error> {
+    let hook_target_ptr = vtable.add(vtable_index);
 
-pub struct IosInterceptor;
+    let orig_addr = ptr::read(hook_target_ptr);
 
-impl Interceptor for IosInterceptor {
-    fn hook(&self, target: usize, detour: usize) -> Result<usize, InterceptorError> {
-        match unsafe { dobby_rs::hook(target as *mut _, detour as *mut _) } {
-            Ok(trampoline) => Ok(trampoline as usize),
-            Err(e) => {
-                error!("Dobby hook failed with code: {:?}", e);
-                Err(InterceptorError::Other)
-            }
-        }
-    }
+    let trampoline_addr = hook(orig_addr, hook_addr)?;
 
-    fn unhook(&self, target: usize) {
-        match unsafe { dobby_rs::unhook(target as *mut _) } {
-            Ok(_) => (),
-            Err(e) => {
-                error!("Dobby unhook failed with code: {:?}", e);
-            }
-        }
-    }
+    let handle = HookHandle {
+        orig_addr,
+        trampoline_addr,
+        hook_type: crate::core::interceptor::HookType::Vtable,
+    };
 
-    fn unhook_all(&self) {
-        warn!("Interceptor::unhook_all() is not implemented for iOS");
+    Ok(handle)
+}
+
+pub fn unhook(hook: &HookHandle) {
+    if let Err(e) = unsafe { dobby_rs::unhook(hook.orig_addr as *mut c_void) } {
+        error!("Failed to unhook function at {:#x}: {}", hook.orig_addr, e);
     }
 }
 
-pub fn find_symbol_by_name(image_name: &str, symbol_name: &str) -> usize {
-    let c_symbol_name = match CString::new(symbol_name) {
+pub fn unhook_vtable(hook: &HookHandle) {
+    unhook(hook)
+}
+
+pub unsafe fn get_vtable_from_instance(instance_addr: usize) -> *mut usize {
+    ptr::read(instance_addr as *const *mut usize)
+}
+
+pub unsafe fn find_symbol_by_name(module: &str, symbol: &str) -> usize {
+
+    if module != "UnityFramework" {
+        warn!("find_symbol_by_name called for unhandled module: {}", module);
+    }
+
+    let c_symbol = match CString::new(symbol) {
         Ok(s) => s,
         Err(e) => {
-            error!("Failed to create CString for symbol {}: {}", symbol_name, e);
+            error!("Failed to create CString for symbol {}: {}", symbol, e);
             return 0;
         }
     };
 
-    if image_name != "UnityFramework" {
-         warn!("find_symbol_by_name called for unhandled image: {}", image_name);
-    }
-
-    let addr = unsafe {
-        libc::dlsym(libc::RTLD_DEFAULT, c_symbol_name.as_ptr())
-    };
+    let addr = libc::dlsym(libc::RTLD_DEFAULT, c_symbol.as_ptr());
 
     if addr.is_null() {
-        error!("Failed to find symbol '{}' in any loaded image.", symbol_name);
+        error!("Failed to find symbol '{}' in any loaded image.", symbol);
         0
+    } else {
+        info!("Found symbol '{}' at address {:p}", symbol, addr);
+        addr as usize
     }
+}
