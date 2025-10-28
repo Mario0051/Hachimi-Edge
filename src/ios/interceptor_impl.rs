859320bee9e002d9e54fee804809ef7c9a4f0158
Mario0051 <windowsmario64@gmail.com>
2025-10-28 19:28:05 +0000

    Update interceptor_impl.rs

--- src/ios/interceptor_impl.rs
+++ src/ios/interceptor_impl.rs
@@ -1,47 +1,33 @@
-use crate::core::error::InterceptorError;
+
+
+use crate::core::interceptor::{Interceptor, InterceptorError};
 use std::ffi::CString;
 
-pub fn hook(target: usize, detour: usize) -> Result<usize, InterceptorError> {
-    let mut trampoline = 0;
-    let result = unsafe {
-        dobby_rs::hook(
-            target as *mut _,
-            detour as *mut _,
-            &mut trampoline as *mut _ as *mut *mut _,
-        )
-    };
+pub struct IosInterceptor;
 
-    if result == dobby_rs::DOBBY_SUCCESS {
-        Ok(trampoline)
-    } else {
-        error!("Dobby hook failed with code: {}", result);
-        Err(InterceptorError::Other)
+impl Interceptor for IosInterceptor {
+    fn hook(&self, target: usize, detour: usize) -> Result<usize, InterceptorError> {
+        match unsafe { dobby_rs::hook(target as *mut _, detour as *mut _) } {
+            Ok(trampoline) => Ok(trampoline as usize),
+            Err(e) => {
+                error!("Dobby hook failed with code: {:?}", e);
+                Err(InterceptorError::Other)
+            }
+        }
     }
-}
 
-pub fn unhook(target: usize) {
-    let result = unsafe { dobby_rs::unhook(target as *mut _) };
-    if result != dobby_rs::DOBBY_SUCCESS {
-        error!("Dobby unhook failed with code: {}", result);
+    fn unhook(&self, target: usize) {
+        match unsafe { dobby_rs::unhook(target as *mut _) } {
+            Ok(_) => (),
+            Err(e) => {
+                error!("Dobby unhook failed with code: {:?}", e);
+            }
+        }
     }
-}
 
-pub fn hook_vtable(
-    _vtable: usize,
-    _vtable_index: usize,
-    _hook_addr: usize,
-) -> Result<usize, InterceptorError> {
-    error!("hook_vtable is not implemented for iOS");
-    Err(InterceptorError::NotImplemented)
-}
-
-pub fn unhook_vtable(_hook_handle: usize) {
-    error!("unhook_vtable is not implemented for iOS");
-}
-
-pub fn get_vtable_from_instance(_instance_addr: usize) -> usize {
-    error!("get_vtable_from_instance is not implemented for iOS");
-    0
+    fn unhook_all(&self) {
+        warn!("Interceptor::unhook_all() is not implemented for iOS");
+    }
 }
 
 pub fn find_symbol_by_name(image_name: &str, symbol_name: &str) -> usize {
@@ -64,8 +50,4 @@ pub fn find_symbol_by_name(image_name: &str, symbol_name: &str) -> usize {
     if addr.is_null() {
         error!("Failed to find symbol '{}' in any loaded image.", symbol_name);
         0
-    } else {
-        info!("Found symbol '{}' at address {:p}", symbol_name, addr);
-        addr as usize
     }
-}
