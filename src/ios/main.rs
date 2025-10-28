d68325ca90a06abd3e9d391bfa58fc7c19d30a1c
Mario <windowsmario64@gmail.com>
2025-10-28 10:14:53 -0700

    Add files via upload

--- src/ios/main.rs
+++ src/ios/main.rs
@@ -1,21 +1,36 @@
-use ctor::ctor;
-use crate::core;
+use std::ffi::{c_void, CStr};
+use std::sync::Once;
 
-use super::{
-    game_impl::IosGame, gui_impl::IosGui, hachimi_impl::IosHachimi,
-    interceptor_impl::IosInterceptor, log_impl::IosLog, symbols_impl::IosSymbols,
-};
+static STARTUP_ONCE: Once = Once::new();
 
-#[ctor]
-fn entrypoint() {
-    std::thread::spawn(|| {
-        core::init(
-            Box::new(IosLog),
-            Box::new(IosHachimi),
-            Box::new(IosGame),
-            Box::new(IosGui),
-            Box::new(IosInterceptor),
-            Box::new(IosSymbols),
-        );
-    });
+#[no_mangle]
+pub unsafe extern "C" fn dlopen(path: *const i8, mode: i32) -> *mut c_void {
+    let real_dlopen: extern "C" fn(*const i8, i32) -> *mut c_void =
+        std::mem::transmute(libc::dlsym(libc::RTLD_NEXT, b"dlopen\0".as_ptr() as _));
+
+    let handle = real_dlopen(path, mode);
+
+    if !path.is_null() {
+        let path_str = CStr::from_ptr(path).to_string_lossy();
+
+        if path_str.contains("UnityFramework") {
+            STARTUP_ONCE.call_once(|| {
+                std::thread::spawn(initialize_hachimi);
+            });
+        }
+    }
+
+    handle
+}
+
+fn initialize_hachimi() {
+    super::log_impl::init(log::LevelFilter::Info);
+    crate::core::init(
+        Box::new(super::log_impl::IosLog),
+        Box::new(super::hachimi_impl::IosHachimi),
+        Box::new(super::game_impl::IosGame),
+        Box::new(super::gui_impl::IosGui),
+        Box::new(super::interceptor_impl::IosInterceptor),
+        Box::new(super::symbols_impl::IosSymbols),
+    );
 }
