51a340c841f5b45cccce71c0ab80701c4e8d27ce
Mario0051 <windowsmario64@gmail.com>
2025-10-28 19:24:16 +0000

    Update interceptor_impl.rs

--- src/ios/interceptor_impl.rs
+++ src/ios/interceptor_impl.rs
@@ -1,37 +1,47 @@
-use crate::core::interceptor::{Interceptor, InterceptorError};
+use crate::core::error::InterceptorError;
 use std::ffi::CString;
 
-pub struct IosInterceptor;
+pub fn hook(target: usize, detour: usize) -> Result<usize, InterceptorError> {
+    let mut trampoline = 0;
+    let result = unsafe {
+        dobby_rs::hook(
+            target as *mut _,
+            detour as *mut _,
+            &mut trampoline as *mut _ as *mut *mut _,
+        )
+    };
 
-impl Interceptor for IosInterceptor {
-    fn hook(&self, target: usize, detour: usize) -> Result<usize, InterceptorError> {
-        let mut trampoline = 0;
-        let result = unsafe {
-            dobby_rs::hook(
-                target as *mut _,
-                detour as *mut _,
-                &mut trampoline as *mut _ as *mut *mut _,
-            )
-        };
-
-        if result == dobby_rs::dobby_errno::DOBBY_SUCCESS {
-            Ok(trampoline)
-        } else {
-            error!("Dobby hook failed with code: {}", result);
-            Err(InterceptorError::Other)
-        }
+    if result == dobby_rs::DOBBY_SUCCESS {
+        Ok(trampoline)
+    } else {
+        error!("Dobby hook failed with code: {}", result);
+        Err(InterceptorError::Other)
     }
+}
 
-    fn unhook(&self, target: usize) {
-        let result = unsafe { dobby_rs::unhook(target as *mut _) };
-        if result != dobby_rs::dobby_errno::DOBBY_SUCCESS {
-            error!("Dobby unhook failed with code: {}", result);
-        }
+pub fn unhook(target: usize) {
+    let result = unsafe { dobby_rs::unhook(target as *mut _) };
+    if result != dobby_rs::DOBBY_SUCCESS {
+        error!("Dobby unhook failed with code: {}", result);
     }
+}
 
-    fn unhook_all(&self) {
-        warn!("Interceptor::unhook_all() is not implemented for iOS");
-    }
+pub fn hook_vtable(
+    _vtable: usize,
+    _vtable_index: usize,
+    _hook_addr: usize,
+) -> Result<usize, InterceptorError> {
+    error!("hook_vtable is not implemented for iOS");
+    Err(InterceptorError::NotImplemented)
+}
+
+pub fn unhook_vtable(_hook_handle: usize) {
+    error!("unhook_vtable is not implemented for iOS");
+}
+
+pub fn get_vtable_from_instance(_instance_addr: usize) -> usize {
+    error!("get_vtable_from_instance is not implemented for iOS");
+    0
 }
 
 pub fn find_symbol_by_name(image_name: &str, symbol_name: &str) -> usize {
@@ -43,11 +53,8 @@ pub fn find_symbol_by_name(image_name: &str, symbol_name: &str) -> usize {
         }
     };
 
-    //
-
     if image_name != "UnityFramework" {
          warn!("find_symbol_by_name called for unhandled image: {}", image_name);
-         return 0;
     }
 
     let addr = unsafe {
