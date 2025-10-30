f812cf729cd819302290a5191eec69a6cdadb850
Mario <windowsmario64@gmail.com>
2025-10-30 09:51:40 -0700

    Update build-ios.yml
    Update build.rs
    
    Update mod.rs

--- src/ios/mod.rs
+++ src/ios/mod.rs
@@ -1,6 +1,18 @@
 #[allow(dead_code, non_upper_case_globals, non_camel_case_types, non_snake_case)]
 pub mod titanox {
     include!(concat!(env!("OUT_DIR"), "/titanox_bindings.rs"));
+
+    #[allow(non_camel_case_types)]
+    pub type TXStatus = ::std::os::raw::c_int;
+    pub const TX_SUCCESS: TXStatus = 0;
+
+    extern "C" {
+        pub fn TXHookFunction(
+            target: *mut ::std::ffi::c_void,
+            replacement: *mut ::std::ffi::c_void,
+            original: *mut *mut ::std::ffi::c_void,
+        ) -> TXStatus;
+    }
 }
 
 pub mod game_impl;
