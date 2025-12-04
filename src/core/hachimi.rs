c09db31d0d257bc94c8f02afa91125afdbaa0549
Mario <windowsmario64@gmail.com>
2025-12-04 02:30:50 -0800

    User configurable GUI scaling.

--- src/core/hachimi.rs
+++ src/core/hachimi.rs
@@ -276,6 +276,8 @@ pub struct Config {
     pub disable_auto_update_check: bool,
     #[serde(default)]
     pub disable_translations: bool,
+    #[serde(default = "Config::default_gui_scale")]
+    pub gui_scale: f32,
     #[serde(default = "Config::default_ui_scale")]
     pub ui_scale: f32,
     #[serde(default)]
@@ -324,6 +326,7 @@ impl Config {
     fn default_open_browser_url() -> String { "https://www.google.com/".to_owned() }
     fn default_virtual_res_mult() -> f32 { 1.0 }
     fn default_ui_scale() -> f32 { 1.0 }
+    fn default_gui_scale() -> f32 { 1.0 }
     fn default_story_choice_auto_select_delay() -> f32 { 1.2 }
     fn default_story_tcps_multiplier() -> f32 { 3.0 }
     fn default_meta_index_url() -> String { "https://gitlab.com/umatl/hachimi-meta/-/raw/main/meta.json".to_owned() }
